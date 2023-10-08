f5cacea40ecc1d4ff7911250bd6855aec59bfe8a
Expliyh <expliyh@outlook.com>
2023-10-08 23:03:54 +0800

    Update

---
+++ src/main/java/top/expli/schoolfish/ItemOrder.java
@@ -0,0 +1,40 @@
+package top.expli.schoolfish;
+
+public class ItemOrder implements Order {
+
+    /**
+     */
+    @Override
+    public String getSellerID() {
+        return null;
+    }
+
+    /**
+     */
+    @Override
+    public String getBuyerID() {
+        return null;
+    }
+
+    /**
+     */
+    @Override
+    public String getItemID() {
+        return null;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void getItemSnapshot() {
+
+    }
+
+    /**
+     */
+    @Override
+    public int getOrderStat() {
+        return 0;
+    }
+}
--- src/main/java/top/expli/schoolfish/Order.java
+++ src/main/java/top/expli/schoolfish/Order.java
@@ -2,8 +2,19 @@ package top.expli.schoolfish;
 
 
 public interface Order {
+    /**
+     * Get seller's UserID.
+     * @author expliyh
+     * @return Seller's ID
+     * @since 0.0
+     */
     String getSellerID();
-
+    /**
+     * Get buyer's UserID.
+     * @author expliyh
+     * @return Buyer's ID
+     * @since 0.0
+     */
     String getBuyerID();
 
     String getItemID();
