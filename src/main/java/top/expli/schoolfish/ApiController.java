160c5bc95c0345cc9c9665ff648682f4f0c8f8c3
Expliyh <expliyh@outlook.com>
2023-10-10 13:55:23 +0800

    更新文档

--- src/main/java/top/expli/schoolfish/ApiController.java
+++ src/main/java/top/expli/schoolfish/ApiController.java
@@ -4,8 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * HTTP API 接口的路由类，在此处配置 API 路由.
+ */
 @Controller
 public class ApiController {
+    /**
+     * 服务端程序测试路由，如果在运行就会返回 Hello, World!
+     * @return 返回 Hello, World! 证明服务端程序正在运行
+     */
     @RequestMapping({"/hello", "/ping"})
     @ResponseBody
     public String hello() {
