0895602980faa744c007820f06476d0ae02aaaee
Expliyh <expliyh@outlook.com>
2023-10-10 16:07:19 +0800

    修正未 break 的 switch

--- src/main/java/top/expli/schoolfish/OrderManager.java
+++ src/main/java/top/expli/schoolfish/OrderManager.java
@@ -7,9 +7,10 @@ public class OrderManager {
     /**
      * 下订单的函数.
      * 在请求 HTTP 下订单 API 时会调用此函数。
-     * @param itemID 订单交易物品的 ID
+     *
+     * @param itemID   订单交易物品的 ID
      * @param sellerID 卖家的 UID
-     * @param buyerID 买家的 UID
+     * @param buyerID  买家的 UID
      * @return 字符串形式的订单号，最后的 @ 之后的数字代表订单类型
      */
     public static String placeOrder(String itemID, String sellerID, String buyerID) {
@@ -17,10 +18,12 @@ public class OrderManager {
         Order order;
         String orderID = "";
         switch (itemType) {
-            case 0:
+            case 0 -> {
                 order = new ItemOrder(sellerID, buyerID, itemID);
                 order.setOrderStatus(OrderStats.PLACED);
                 orderID = Database.addItemOrder((ItemOrder) order);
+            }
+
         }
         return orderID;
     }
