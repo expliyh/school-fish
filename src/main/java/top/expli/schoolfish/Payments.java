f040186167b98bb2ce83c3e029af640dc6330b57
Expliyh <expliyh@outlook.com>
2023-10-10 14:52:58 +0800

    完善文档

--- src/main/java/top/expli/schoolfish/Payments.java
+++ src/main/java/top/expli/schoolfish/Payments.java
@@ -11,23 +11,50 @@ public class Payments {
 }
 
 class Payment {
+    /**
+     * 支付平台的订单号.
+     */
     private String platformID;
+    /**
+     * 本平台的订单号.
+     */
     private String orderID;
+    /**
+     * 支付平台签名，用于验证回调是否合法.
+     */
     private String sign;
-
+    /**
+     * 订单超时时间，默认15分钟.
+     */
     private static final long TIMEOUT = 15 * 60 * 1000; // 15 minutes
-
+    /**
+     * 订单创建时间，使用 System.currentTimeMillis() 获取.
+     * 该值应与支付平台保持一致或略大于支付平台设置.
+     */
     private long createTime;
 
+    /**
+     * 判断订单是否超时.
+     * @return 订单是否超时，超时为 True，未超时为 False.
+     */
     public boolean isTimedOut() {
         long currentTime = System.currentTimeMillis();
         return (currentTime - createTime) > TIMEOUT;
     }
 
+    /**
+     * 重设超时时间。
+     */
     private void resetTimeout() {
         createTime = System.currentTimeMillis();
     }
 
+    /**
+     * 构造函数.s
+     * @param platformID 支付平台的订单号.
+     * @param orderID 本平台的订单号.
+     * @param sign 支付平台签名，用于验证回调是否合法.
+     */
     public Payment(String platformID, String orderID, String sign) {
         this.platformID = platformID;
         this.orderID = orderID;
