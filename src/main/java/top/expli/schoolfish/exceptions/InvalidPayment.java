be2e2e8bebf2fc5286fdc41fbd810092705d90b6
Expliyh <expliyh@outlook.com>
2023-10-10 16:13:04 +0800

    自定义异常：无效的支付异常

---
+++ src/main/java/top/expli/schoolfish/exceptions/InvalidPayment.java
@@ -0,0 +1,26 @@
+package top.expli.schoolfish.exceptions;
+
+/**
+ * 无效的支付异常
+ */
+public class InvalidPayment extends Exception{
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public InvalidPayment() {
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public InvalidPayment(String message) {
+        super(message);
+    }
+}
