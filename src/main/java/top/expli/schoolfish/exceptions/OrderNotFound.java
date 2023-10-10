f6b4ba680e146c27d38abbe471ddd36d17740d0d
Expliyh <expliyh@outlook.com>
2023-10-10 16:06:53 +0800

    自定义异常：找不到订单异常

---
+++ src/main/java/top/expli/schoolfish/exceptions/OrderNotFound.java
@@ -0,0 +1,26 @@
+package top.expli.schoolfish.exceptions;
+
+/**
+ * 找不到订单异常
+ */
+public class OrderNotFound extends Exception{
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public OrderNotFound() {
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public OrderNotFound(String message) {
+        super(message);
+    }
+}
