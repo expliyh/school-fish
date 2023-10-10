727b6c986dc2d2ce594e5b26709f3271565b8f46
Expliyh <expliyh@outlook.com>
2023-10-10 15:53:07 +0800

    添加更新订单的方法
    更新订单和更新物品订单

--- src/main/java/top/expli/schoolfish/Database.java
+++ src/main/java/top/expli/schoolfish/Database.java
@@ -21,6 +21,28 @@ public class Database {
         ItemOrder savedOrder = itemOrderRepository.save(order);
         return savedOrder.getOrderID();
     }
+
+    /**
+     * 更新数据库中的物品订单.
+     * @param order 物品订单实体
+     * @return 字符串形式的订单号
+     */
+    public static String updateItemOrder(ItemOrder order){
+        ItemOrder savedOrder = itemOrderRepository.save(order);
+        return savedOrder.getOrderID();
+    }
+
+    /**
+     * 更新数据库中的订单（自适应类型）
+     * @param order 订单实体
+     * @return 字符串形式的订单号
+     */
+    public static String updateOrder(Order order){
+        if (order instanceof ItemOrder){
+            return updateItemOrder((ItemOrder) order);
+        }
+        return "";
+    }
 }
 
 interface ItemOrderRepository extends JpaRepository<ItemOrder, Long> {
