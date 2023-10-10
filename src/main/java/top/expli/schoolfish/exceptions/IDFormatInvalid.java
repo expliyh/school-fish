0a84a369a804298c9a356da34bab48e8f662a22c
Expliyh <expliyh@outlook.com>
2023-10-10 15:58:47 +0800

    自定义异常：ID格式无效异常

---
+++ src/main/java/top/expli/schoolfish/exceptions/IDFormatInvalid.java
@@ -0,0 +1,26 @@
+package top.expli.schoolfish.exceptions;
+
+/**
+ * ID格式无效异常
+ */
+public class IDFormatInvalid extends Exception{
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public IDFormatInvalid() {
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public IDFormatInvalid(String message) {
+        super(message);
+    }
+}
