9b5ccb21f22f48dd4269c79a93f566e77deb0cdb
Expliyh <expliyh@outlook.com>
2023-10-11 12:13:35 +0800

    Update OrderStats.java
    完善注释

--- src/main/java/top/expli/schoolfish/OrderStats.java
+++ src/main/java/top/expli/schoolfish/OrderStats.java
@@ -4,13 +4,40 @@
 package top.expli.schoolfish;
 
 public class OrderStats {
+    /**
+     * 未定义的订单状态
+     */
     public static final int UNDEFINED = -1;
+    /**
+     * 已下单未支付
+     */
     public static final int PLACED = 0;
+    /**
+     * 订单已支付
+     */
     public static final int PAID = 1;
+    /**
+     * 订单已发货
+     */
     public static final int SHIPPED = 2;
+    /**
+     * 订单已签收
+     */
     public static final int SIGNED = 3;
+    /**
+     * 订单已完成但未评价
+     */
     public static final int NOT_REVIEW = 4;
+    /**
+     * 订单已完成并评价
+     */
     public static final int DONE = 5;
+    /**
+     * 订单关闭 (未完成)
+     */
     public static final int CLOSED = 100;
+    /**
+     * 订单售后中
+     */
     public static final int AFTER_SALE = 200;
 }
